1d3a971c80e36109883e364fe52c8d95efc1cc8c
aukdev <amilaupul344@outlook.com>
2023-04-19 13:46:39 +0530

    set firebase for protected router

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,5 +1,22 @@
+import { useDispatch } from "react-redux";
+import { removeUser } from "../../Store/ReduxSlice/userSlice";
+import { useNavigate } from "react-router-dom";
+
 const Admin = () => {
-  return <div>Admin</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logout = () => {
+    dispatch(removeUser());
+    navigate("/admin");
+  };
+  return (
+    <div>
+      <p>Admin</p>
+      <br />
+      <button onClick={logout}>log out</button>
+    </div>
+  );
 };
 
 export default Admin;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,40 +1,107 @@
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
-import db from "../../Firebase/firebase";
+import db, { auth } from "../../Firebase/firebase";
 import { addUser } from "../../Store/ReduxSlice/userSlice";
 import { useNavigate } from "react-router-dom";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 const Login = () => {
   const dispatch = useDispatch();
 
-  const username = useRef();
-  const password = useRef();
+  const usernameRef = useRef();
+  const passwordRef = useRef();
 
   const navigate = useNavigate();
 
-  const loginSubmit = () => {
-    const q = query(collection(db, "users"), where("username", "==", "admin"));
+  useEffect(() => {
+    const fun = () => {
+      const user = auth.currentUser;
+      if (user) {
+        console.log(user);
+      } else {
+        console.log("no user");
+      }
+    };
 
-    getDocs(q)
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-          dispatch(addUser(doc.data()));
-        });
-        navigate("/admin");
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-      });
+    fun();
+
+    setTimeout(() => {
+      console.log("time out");
+      fun();
+    }, 1000);
+  }, []);
+
+  const loginSubmit = () => {
+    const email = usernameRef.current.value;
+    const password = passwordRef.current.value;
+
+    const user = auth.currentUser;
+
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      // ...
+      console.log(user);
+    } else {
+      // No user is signed in.
+      console.log("no user");
+    }
+
+    // signInWithEmailAndPassword(auth, email, password)
+    //   .then((userCredential) => {
+    //     // Signed in
+    //     const user = userCredential.user;
+    //     console.log(user);
+    //     // ...
+    //   })
+    //   .catch((error) => {
+    //     // const errorCode = error.code;
+    //     const errorMessage = error.message;
+
+    //     console.log(errorMessage);
+    //   });
+
+    // const q = query(collection(db, "users"), where("username", "==", "admin"));
+
+    // getDocs(q)
+    //   .then((querySnapshot) => {
+    //     querySnapshot.forEach((doc) => {
+    //       // doc.data() is never undefined for query doc snapshots
+    //       console.log(doc.id, " => ", doc.data());
+    //       dispatch(addUser(doc.data()));
+    //     });
+    //     navigate("/admin");
+    //   })
+    //   .catch((error) => {
+    //     console.log("Error getting documents: ", error);
+    //   });
   };
 
   return (
     <div>
-      <input ref={username} type="text" placeholder="username" />
-      <input ref={password} type="password" placeholder="password" />
+      <input ref={usernameRef} type="email" placeholder="email" />
+      <input ref={passwordRef} type="password" placeholder="password" />
       <button onClick={loginSubmit}>Login</button>
+      <button
+        onClick={() => {
+          signOut(auth)
+            .then(() => {
+              // Sign-out successful.
+              alert("user sign out");
+            })
+            .catch((error) => {
+              // An error happened.
+              alert(error.message);
+            });
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
